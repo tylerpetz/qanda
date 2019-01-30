35a643e4757cf38d3b08e3fbfd29b598be9efff6
Tyler Petz <tylerpetz@gmail.com>
2019-01-29 20:35:46 -0500

    built first test

---
+++ src/components/QA/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    List
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ListSets',
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Questionaire Builder
+          </h1>
+        </div>
+      </div>
+    </section>
+    <NewSet />
+    <EditSet />
+    <ListSets />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NewSet from '@/components/QA/New.vue';
+import EditSet from '@/components/QA/Edit.vue';
+import ListSets from '@/components/QA/List.vue';
 
 export default Vue.extend({
   name: 'home',
+  components: {
+    NewSet,
+    EditSet,
+    ListSets,
+  },
 });
 </script>
